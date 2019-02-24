40e5cf326b2b18c979e6a15269df3d8d7ab7c249
DESKTOP-D2V1UJK\Danilshik <danilshik@yandex.ru>
2019-02-24 22:58:49 +0300

    Реализована функция для очередного сайта

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ from urllib.parse import urljoin
 from dateutil.relativedelta import *
 from dateutil.rrule import *
 from datetime import *
-
+import json
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:61.0) Gecko/20100101 Firefox/61.0'}
 
 def last_weekday(str_month):
@@ -195,10 +195,114 @@ def www32_top_ru(id, type, url_page):
     pprint.pprint(main_dict)
     return main_dict
 
+def www_kleos_ru(id, type):
+    """
+
+    :param id: id специалиста или организации
+    :param type: или doctor или clinic
+    :return:
+    """
+
+    is_last = False
+    # Cчетчики
+    count_positive_comments = 0
+    count_negative_comments = 0
+    count_neitral_comments = 0
+    comment_list = []
+    comment_id_list = []
+
+    if id is None:
+        raise Exception("Не указан id")
+
+
+    if type is "clinic":
+        item_name = "org"
+    elif type is "doctor":
+        item_name = "specialist"
+    else:
+        raise Exception("Указан неправильный type")
+
+    while is_last != True:
+        params = {'last_ids[]': comment_id_list}
+        r = requests.request("GET", "https://www.kleos.ru/comment/0/operation?mode=getlist&item_id=" + str(id) + "&item_name=" + item_name + "&comment_cnt=1000000",
+                             headers=headers, params=params).content
+        html = get_html(r)
+        json_text = json.loads(html.select_one("p").text)
+        html_json = get_html(json_text["html"])
+        if(json_text["is_last"] == True):
+            is_last = True
+        items = html_json.select("body > div.comment-item")
+        print("Количество отзывов:", len(items))
+        # items= items[:1]
+        for item in items:
+            subcomments = item.select("div.comment-item")
+            if(len(subcomments) > 0):
+                response = "yes"
+            else:
+                response = "no"
+            comment_id = item.get("data-id")
+            comment_id_list.append(comment_id)
+            left_block = item.select_one("div.left-content > div")
+            spans = left_block.select("span")
+
+            date = spans[0].text.strip().replace(".", "-")
+
+            author_name = spans[1].text.strip()
+
+            try:
+                emotion_text = float(left_block.select_one("div.star-rating-svg").get("data-initial-rating"))
+                if(emotion_text >= 4.5):
+                    emotion = "positive"
+                    count_positive_comments += 1
+
+                elif(emotion_text <= 2):
+                    emotion = "negative"
+                    count_negative_comments += 1
+                else:
+                    emotion = "neutral"
+                    count_neitral_comments += 1
+            except:
+                emotion = None
+
+            text = item.select_one("div.comment-text.padding-coment").text.strip()
+            url = None
+
+            comment = {
+                'author_name': author_name,
+                'date': date,
+                'emotion': emotion,
+                'text': text,
+                'response': response,
+                'url': url
+            }
+            print(comment)
+            comment_list.append(comment)
+
+    statistic = {
+        'count': count_positive_comments + count_negative_comments + count_neitral_comments,
+        'positive': count_positive_comments,
+        'negative': count_negative_comments,
+        'neutral': count_neitral_comments
+    }
+    main_dict = {
+        'statistic': statistic,
+        'comments': comment_list
+    }
+
+    pprint.pprint(main_dict)
+    return main_dict
+
+
+
+
+
 def all_parsers():
     # www32_top_ru(10545, "doctor", "https://www.32top.ru/dr/10545-stepanov-andrey-vasilevich/")
-    www32_top_ru(10545, "doctor", None)
+    # www32_top_ru(10545, "doctor", None)
     # www32_top_ru(None, None, "https://www.32top.ru/dr/10545-stepanov-andrey-vasilevich/")
+
+    www_kleos_ru(7493, "clinic")
+    # www_kleos_ru(1696, "doctor")
 
 
 if __name__ == '__main__':
