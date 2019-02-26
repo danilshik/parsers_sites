26232d470648db75edc8e7e0faa7d1764836f7a2
DESKTOP-D2V1UJK\Danilshik <danilshik@yandex.ru>
2019-02-26 16:35:16 +0300

    Реализован следующий сайт

--- parser.py
+++ parser.py
@@ -367,6 +367,96 @@ def startsmile_ru(url_page):
     pprint.pprint(main_dict)
     return main_dict
 
+def yell_ru(id):
+    """
+
+    :param id: идентификатор клиники, парсинг происходит по разбору Ajax запросу
+    :return:
+    """
+    url_page = "https://www.yell.ru/company/reviews/"
+    page = 1
+    count_positive_comments = 0
+    count_negative_comments = 0
+    count_neitral_comments = 0
+    comment_list = []
+    while True:
+        params = {
+            "id" : str(id),
+            "page" : page,
+            "sort" : "recent"
+        }
+        r = requests.request("GET", url_page, params=params).content
+        print("Pagination", page)
+        html = get_html(r)
+        # print(html)
+        items = html.select("div.reviews__item")
+        if (len(items) == 0):
+            break
+        for item in items:
+            try:
+                date = item.select_one("span.reviews__item-added").text.strip()
+                date_block = date.split(" ")
+                day = date_block[0]
+                month = MonthRefactor(date_block[1])
+                year = date_block[2]
+                date = year + "-" + month + "-" + day
+            except:
+                date = None
+
+            author_name = item.select_one("div.reviews__item-user-name").text.strip()
+
+            try:
+                emotion_text = float(item.select_one("span.rating__value").text.strip())
+                if (emotion_text >= 4.5):
+                    emotion = "positive"
+                    count_positive_comments += 1
+
+                elif (emotion_text <= 2):
+                    emotion = "negative"
+                    count_negative_comments += 1
+                else:
+                    emotion = "neutral"
+                    count_neitral_comments += 1
+            except Exception as e:
+                emotion = None
+                print(e)
+            response_block = item.select_one("div.repliesreplies_theme_light.ng-scope > div")
+            if response_block is None:
+                response = "no"
+            else:
+                response = "yes"
+
+            text = item.select_one("div.reviews__item-text").text.strip()
+            url = item.select_one("div.share").get("data-url")
+
+            comment = {
+                'author_name': author_name,
+                'date': date,
+                'emotion': emotion,
+                'text': text,
+                'response': response,
+                'url': url
+            }
+            print(comment)
+            comment_list.append(comment)
+
+
+        page += 1
+
+    statistic = {
+        'count': count_positive_comments + count_negative_comments + count_neitral_comments,
+        'positive': count_positive_comments,
+        'negative': count_negative_comments,
+        'neutral': count_neitral_comments
+    }
+    main_dict = {
+        'statistic': statistic,
+        'comments': comment_list
+    }
+
+    pprint.pprint(main_dict)
+    return main_dict
+
 
 
 
@@ -378,8 +468,8 @@ def all_parsers():
 
     # www_kleos_ru(7493, "clinic")
     # www_kleos_ru(1696, "doctor")
-    startsmile_ru("https://www.startsmile.ru/stomatologi/akhtanin_aleksandr_pavlovich.html")
-
+    # startsmile_ru("https://www.startsmile.ru/stomatologi/akhtanin_aleksandr_pavlovich.html")
+    yell_ru(8980641)
 
 if __name__ == '__main__':
     all_parsers()
